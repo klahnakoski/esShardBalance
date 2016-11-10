571f2b60de3bd51033b5ea9ad9525057ca17825b
Kyle Lahnakoski <klahnakoski@mozilla.com>
2016-11-10 08:07:35 -0500

    is nohup ruining ssh calls?

--- resources/scripts/staging/balance.sh
+++ resources/scripts/staging/balance.sh
@@ -4,7 +4,7 @@
 
 cd ~/esShardBalancer
 export PYTHONPATH=.
-nohup python27 balance.py --settings=resources/config/staging/balance.json &
+python27 balance.py --settings=resources/config/staging/balance.json &
 echo $! > run.pid
 disown -h
-tail -n200 -f nohup.out
+tail -n200 -f logs/balance.log
