dfad386a56e4af44ae2e5dad1b180a38f20d4323
Kyle Lahnakoski <klahnakoski@mozilla.com>
2016-11-10 08:37:33 -0500

    fuse dev/null

--- resources/scripts/staging/balance.sh
+++ resources/scripts/staging/balance.sh
@@ -4,7 +4,7 @@
 
 cd ~/esShardBalancer
 export PYTHONPATH=.
-python27 balance.py --settings=resources/config/staging/balance.json &
+python27 balance.py --settings=resources/config/staging/balance.json >& /dev/null < /dev/null &
 echo $! > run.pid
 disown -h
 tail -n200 -f logs/balance.log
